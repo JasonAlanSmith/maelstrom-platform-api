97078876f34a75e6230ec63bfec3d63616eab18e
JasonAlanSmith <smith.jason.alan.me@gmail.com>
2023-10-10 21:23:10 -0500

    tools: update curl script to delete issues

--- scripts/curl/delete_issue.sh
+++ scripts/curl/delete_issue.sh
@@ -1,2 +1,4 @@
 
-curl -d '{"SysId":2}' -H "Content-Type: application/json" -X DELETE http://localhost:8080/issue/2
+curl -d '{"SysId":"25b1e783-6a4f-4825-afb8-c667eaa195ce"}' -H "Content-Type: application/json" -X DELETE http://localhost:8080/issue/25b1e783-6a4f-4825-afb8-c667eaa195ce
+curl -d '{"SysId":"38d946c4-a67a-4d05-9071-acde4d50665f"}' -H "Content-Type: application/json" -X DELETE http://localhost:8080/issue/38d946c4-a67a-4d05-9071-acde4d50665f
+
